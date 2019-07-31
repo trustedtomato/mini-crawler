a643dafabfd7e060e608de2d6b50e933f6349e09
Tamas Halasi <trusted.tomato@gmail.com>
2019-07-31 20:44:39 +0200

    add drain event, callbacks and tests

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const request = require('request');
 const { URL } = require('url');
+const { EventEmitter } = require('events');
 
-module.exports = class Crawler {
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+module.exports = class Crawler extends EventEmitter {
   constructor({
     gap = 0,
     maxConnections = 1,
     absoluteUrl,
-  }, handler) {
+    handler,
+  }) {
+    super();
+    if (typeof handler === 'undefined') throw new Error('Handler is undefined!');
     this.absoluteUrl = absoluteUrl;
     this.gap = gap;
     this.maxConnections = maxConnections;
@@ -15,32 +21,47 @@ module.exports = class Crawler {
     this.running = 0;
   }
 
-  queue(url, options) {
+  queue({
+    url,
+    params,
+    callback,
+  }) {
+    if (typeof url === 'undefined') throw new Error('Queued URL is undefined!');
+    if (typeof callback !== 'undefined' && typeof callback !== 'function') throw new Error('Callback must be a function!');
     this.urlQueue.push({
       url,
-      options,
+      params,
+      callback,
     });
     this.tryQueueShift();
   }
 
   tryQueueShift() {
-    if (this.running < this.maxConnections) {
+    if (this.running < this.maxConnections && this.urlQueue.length > 0) {
       this.running += 1;
-      const { url, options } = this.urlQueue.shift();
+      const { url, params, callback } = this.urlQueue.shift();
       request(
         new URL(url, this.absoluteUrl).href,
         (err, response, body) => {
           Promise.resolve(
             this.handler(err, {
               ...response,
-              options,
+              params,
               body,
             }),
-          ).then(() => {
-            setTimeout(() => {
-              this.running -= 1;
+          ).then(async (result) => {
+            if (callback) {
+              await callback(result);
+            }
+            if (this.gap > 0) {
+              await wait(this.gap);
+            }
+            this.running -= 1;
+            if (this.urlQueue.length === 0 && this.running === 0) {
+              this.emit('drain');
+            } else {
               this.tryQueueShift();
-            }, this.gap);
+            }
           });
         },
       );
--- index.test.js
+++ index.test.js
@@ -1,20 +1,80 @@
 const Crawler = require('.');
 
-test('crawls Wikipedia', (done) => {
-  const articles = [];
+test('crawls Wikipedia (absoluteUrl + maxConnections + callback)', (done) => {
   const crawler = new Crawler({
     absoluteUrl: 'https://hu.wikipedia.org/',
-  }, (err, { body }) => {
-    const titleRegex = /<title>(.*?) – Wikipédia<\/title>/;
-    const articleUrlRegex = /href="(\/wiki\/.*?)"/g;
-    articles.push(body.match(titleRegex)[1].trim());
-    if (articles.length === 3) {
-      done();
-    }
-    let articleUrlMatch;
-    while ((articleUrlMatch = articleUrlRegex.exec(body)) !== null) {
-      crawler.queue(articleUrlMatch[1]);
-    }
+    maxConnections: 2,
+    handler: (err, { body }) => {
+      const titleRegex = /<title>(.*?) – Wikipédia<\/title>/;
+      const articleUrlRegex = /href="(\/wiki\/.*?)"/g;
+      const references = [];
+      while ((articleUrlMatch = articleUrlRegex.exec(body)) !== null) {
+        references.push(articleUrlMatch[1]);
+      }
+      return {
+        title: body.match(titleRegex)[1].trim(),
+        references,
+      };
+    },
   });
-  crawler.queue('/wiki/Csaknekedkisl%C3%A1ny');
+  crawler.queue({
+    url: '/wiki/Csaknekedkisl%C3%A1ny',
+    callback: ({ title, references }) => {
+      expect(title).toBe('Csaknekedkislány');
+      references.slice(0, 4).forEach((reference) => {
+        crawler.queue({ url: reference });
+      });
+      expect(crawler.urlQueue.length).toBe(3);
+      expect(crawler.running).toBe(2);
+      done();
+    },
+  });
+});
+
+test('crawls Ultimate-guitar (drain + callback)', (done) => {
+  const artistUrls = [];
+  const artistCrawler = new Crawler({
+    gap: 1000,
+    handler: async (err, { body }) => {
+      if (err) {
+        throw err;
+      }
+      const ugappStorePage = body.match(/UGAPP\.store\.page\s*=\s*(\{.*\});\n\s+/);
+      const page = JSON.parse(ugappStorePage[1]);
+      const urls = page.data.artists.map(({ artist_url: url }) => url);
+      artistUrls.push(...urls);
+      return urls;
+    },
+  });
+
+  artistCrawler.on('drain', () => {
+    expect(artistUrls.length > 0).toBe(true);
+    done();
+  });
+
+  artistCrawler.queue({
+    url: 'https://www.ultimate-guitar.com/bands/a.htm',
+    callback: (result1) => {
+      expect(result1.length).toBeGreaterThan(0);
+      expect(result1).toContain('/artist/a_b_the_sea_28115');
+      artistCrawler.queue({
+        url: 'https://www.ultimate-guitar.com/bands/a10000.htm',
+        callback: (result2) => {
+          expect(result2.length).toBe(0);
+        },
+      });
+    },
+  });
+});
+
+test('sends nothing (error)', () => {
+  expect(() => {
+    new Crawler().queue('nah');
+  }).toThrow(/destruct/i);
+});
+
+test('sends no handler (error)', () => {
+  expect(() => {
+    new Crawler({}).queue('nah');
+  }).toThrow(/Handler/i);
 });
