9befe35d9f2277ce0d5f60992d68de8885d9d645
Tamas Halasi <trusted.tomato@gmail.com>
2020-07-22 15:03:57 +0200

    add coverage config to jest + use absolute paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
+const { join } = require('path')
+
 module.exports = {
-  rootDir: 'built',
+  rootDir: join(__dirname, 'built'),
   // only test JavaScript files
-  testMatch: [ "**/__tests__/**/*.js", "**/?(*.)+(spec|test).js" ]
+  testMatch: [ "**/__tests__/**/*.js", "**/?(*.)+(spec|test).js" ],
+  coverageDirectory: join(__dirname, 'coverage'),
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  }
 }
